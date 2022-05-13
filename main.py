4b0c0684a26ee560d1a8e2d0dcd8d8b0d1748a24
MrF1LI <76873891+MrF1LI@users.noreply.github.com>
2022-05-13 23:43:54 +0400

    001

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import sqlite3
+
+url = "https://api.spaceflightnewsapi.net/v3/articles"
+r = requests.get(url)
+data = r.json()
+
+# Connect to db
+
+conn = sqlite3.connect("news.sqlite")
+cursor = conn.cursor()
+
+
+# Functions
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS news
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title VARCHAR (200),
+                url VARCHAR (1000),
+                newsSite VARCHAR (500),
+                summary VARCHAR (2000));''')
+
+# Save data
+
+for news in data:
+    news_data = (news["title"], news["url"], news["newsSite"], news["summary"])
+    query = "INSERT INTO news (title, url, newsSite, summary) VALUES (?, ?, ?, ?)"
+    cursor.execute(query, news_data)
+    conn.commit()
+
+
+conn.close()  # Disconnect
